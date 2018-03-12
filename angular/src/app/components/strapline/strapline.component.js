c9834fe438932ac2a07a70399506185842671403
idochynets <idochynets@brainence.com>
2018-03-12 19:46:28 +0200

    Implemented js interval based solution for straplines

--- angular/src/app/components/strapline/strapline.component.js
+++ angular/src/app/components/strapline/strapline.component.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Strapline {
-	constructor(){
-		this.line = `Our average time from pickup to drop-off`
+	constructor($interval){
+		this.$inreval = $interval;
+		this.nextInetrval = 1000;
+		this.nextIndex = 0;
+		this.tagLines = [
+			"Our average time from pickup to drop-off",
+    		"Quicker than your commute",
+			"Slow and steady wins the race - not someone from Repairly",
+			"Recently compared to the Hadron Collider",
+			"Repairly representative spotted in Bugatti Veyron, Feb 2016",
+			"Usain Bolt, team leader at Repairly",
+			"So fast it'll blow your socks off"];
+
+		this.startInterval();
+	}
+
+	startInterval(){
+		this.$inreval(() => {
+			this.next();
+		}, this.nextInetrval)
+	}
+
+	next() {
+		this.line = this.tagLines[this.nextIndex % this.tagLines.length];
+		this.nextIndex++;
 	}
 }
 
-Strapline.$inject = [];
+Strapline.$inject = ['$interval'];
 
 export const strapline = {
   controller: Strapline,
   templateUrl: require('ngtemplate!./strapline.html')
 }
-  
